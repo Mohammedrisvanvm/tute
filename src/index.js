700037baa5f331ab6b8aeca8a4054f980c69062b
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-05-23 12:36:29 +0530

    Add multer and uuid for file upload functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import express from "express";
+import cors from "cors";
+import multer from "multer";
+import { v4 as uuidV4 } from "uuid";
+import path from "path";
 
 const app = express();
 
 const PORT = process.env.PORT || 8000;
+
+const store = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + uuidV4() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage: store,
+});
 
 app.use(
   cors({
@@ -11,18 +28,30 @@ app.use(
   })
 );
 
-app.use((req, res, next => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-}));
+app.use(
+  (req,
+  res,
+  (next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads",express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  console.log(req.file);
+  console.log("file uploaded");
 });
 
 app.listen(PORT, () => {
