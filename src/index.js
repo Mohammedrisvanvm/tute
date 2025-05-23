92186a74e3e5fe8c091cd7ca8fd11d9517fc5cbd
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2025-05-23 12:27:54 +0530

    Fix CORS middleware implementation and ensure proper request handling

--- src/index.js
+++ src/index.js
@@ -11,7 +11,14 @@ app.use(
   })
 );
 
+app.use((req, res, next => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+}));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
